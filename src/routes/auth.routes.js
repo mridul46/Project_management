8c2e5b1981caa93be02ee65bbd7a8ce38fe0010e
mridul <mridulvai46@gmail.com>
2025-08-27 19:15:33 +0530

    logout section added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,16 @@
 import {Router} from "express";
-import {login, registerUser} from "../controllers/auth.controllers.js"
+import {login, registerUser,logoutUser} from "../controllers/auth.controllers.js"
 import { validate } from "../middlewares/validator.middlewares.js";
 import { userRegisterValidator,userLoginValidator } from "../validators/index.js";
-
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 const router=Router();
  
 router.route("/register").post(userRegisterValidator(),validate, registerUser);
 
 router.route("/login").post(userLoginValidator(),validate, login);
+
+//Secure routes
+router.route("/logout").post(verifyJWT,logoutUser);
 
 
 
