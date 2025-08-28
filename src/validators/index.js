97b5acdbbaabd836942f234d280a26d6a6d04698
mridul <mridulvai46@gmail.com>
2025-08-28 15:26:07 +0530

    update auth routes

--- src/validators/index.js
+++ src/validators/index.js
@@ -42,7 +42,41 @@ const userLoginValidator=()=>{
 
     ];
 }
+
+const userChangeCurrentpasswordValidator=()=>{
+    return [
+        body("oldPassword")
+        .notEmpty()
+        .withMessage("old password is required"),
+
+
+        body("newPassword")
+        .notEmpty()
+        .withMessage("New Password is required"),
+    ];
+}
+
+const userForgotPasswordValidator=()=>{
+    return [
+        body("email")
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("Email is invalid")
+    ]
+}
+
+const userResetForgotPasswordValidator=()=>{
+    return [
+        body("newPassword")
+        .notEmpty()
+        .withMessage("Password is required")
+    ]
+}
 export {
     userRegisterValidator,
     userLoginValidator,
+    userChangeCurrentpasswordValidator,
+    userForgotPasswordValidator,
+    userResetForgotPasswordValidator,
 }
